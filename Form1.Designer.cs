a1fee38c1ed3a35f896182499a43d75bd5b273b5
koktoh <koktoh96@gmail.com>
2015-08-27 22:36:43 +0900

    初コミット

---
+++ Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Hibiki_Angel_Generator
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.inputTextBox = new System.Windows.Forms.TextBox();
+			this.startButton = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// inputTextBox
+			// 
+			this.inputTextBox.Location = new System.Drawing.Point(12, 12);
+			this.inputTextBox.Name = "inputTextBox";
+			this.inputTextBox.Size = new System.Drawing.Size(340, 19);
+			this.inputTextBox.TabIndex = 0;
+			// 
+			// startButton
+			// 
+			this.startButton.Location = new System.Drawing.Point(277, 37);
+			this.startButton.Name = "startButton";
+			this.startButton.Size = new System.Drawing.Size(75, 23);
+			this.startButton.TabIndex = 1;
+			this.startButton.Text = "生成！";
+			this.startButton.UseVisualStyleBackColor = true;
+			this.startButton.Click += new System.EventHandler(this.startButton_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(364, 67);
+			this.Controls.Add(this.startButton);
+			this.Controls.Add(this.inputTextBox);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "Form1";
+			this.Text = "響ちゃんマジ天使言語ジェネレーター";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox inputTextBox;
+		private System.Windows.Forms.Button startButton;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Hibiki_Angel_Generator
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void startButton_Click(object sender, EventArgs e)
+		{
+			generate(inputTextBox.Text);
+		}
+
+		private void generate(string src)
+		{
+			if (string.IsNullOrWhiteSpace(src))
+			{
+				MessageBox.Show("文字を入れてください");
+				return;
+			}
+
+			/*
+			List<char> cList = new List<char>();
+			int min = 127;
+			int max = 0;
+
+			foreach (char c in src)
+			{
+				min = Math.Min(min, c);
+				max = Math.Max(max, c);
+				if (!cList.Contains(c))
+					cList.Add(c);
+			}
+
+			cList.Sort();
+			*/
+
+			string result = string.Empty;
+
+			for (int i = 0; i < 10; i++)
+				result += "響ちゃん";
+
+			result += "マジ天使";
+
+			foreach (char c in src)
+			{
+				for (int i = 0; i < c / 10; i++)
+					result += "響ちゃん";
+				result += "天使";
+			}
+
+			result = result.Substring(0, result.Length - 2);
+
+			for (int i = 0; i < src.Length; i++)
+				result += "結婚しよ";
+
+			result += "かわいいかなさんどー天使";
+
+			foreach (char c in src)
+			{
+				for (int i = 0; i < c % 10; i++)
+					result += "響ちゃん";
+				result += "天使";
+			}
+
+			result = result.Substring(0, result.Length - 2);
+
+			for (int i = 0; i < src.Length; i++)
+				result += "結婚しよ";
+
+			for (int i = 0; i < src.Length; i++)
+				result += "天使愛してる";
+
+			output(result);
+		}
+
+		private void output(string s)
+		{
+			SaveFileDialog sfd = new SaveFileDialog();
+
+			sfd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+			sfd.FileName = "Noname";
+			sfd.Filter = "響ちゃんマジ天使言語ファイル(*.hcmt)|*.hcmt|テキストファイル(*.txt)|*.txt";
+			sfd.Title = "どこにファイルを保存する？";
+			sfd.RestoreDirectory = true;
+
+			if (sfd.ShowDialog() == DialogResult.OK)
+			{
+				Stream stream = sfd.OpenFile();
+				if (stream != null)
+				{
+					StreamWriter sw = new StreamWriter(stream);
+					sw.Write(s);
+					sw.Close();
+				}
+				stream.Close();
+
+			}
+
+			sfd.Dispose();
+		}
+	}
+}
